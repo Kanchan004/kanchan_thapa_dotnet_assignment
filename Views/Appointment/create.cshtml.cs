78a471467d0a1262bb2957ddde5c279e9c537042
Kanchan004 <133563972+Kanchan004@users.noreply.github.com>
2025-02-14 21:55:58 +0545

    Add files via upload

---
+++ Views/Appointment/create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Appointmentschedular.Views.Appointment
+{
+    public class createModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
